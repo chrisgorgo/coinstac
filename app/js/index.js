0934bcf51021ca68c49539068fcce7f87cbf85c2
Cory Reed <creed@mrn.org>
2015-07-14 11:20:33 -0700

    Change Bootstrap styles to be loaded via Webpack.

--- app/js/index.js
+++ app/js/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 import React from 'react';
-import Router from 'react-router';
 import routes from './routes';
 
-let RouteHandler = Router.RouteHandler;
+// Load application stylesheets
+require('../styles/app.scss');
 
 routes.run((Root) => {
-    React.render(<Root />, document.body);
+    React.render(<Root />, document.getElementById('app'));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,41 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  context: __dirname + '/app',
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './js/index.js'
-  ],
-  output: {
-    path: __dirname + '/app', //path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-    // publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'app/js/')
-    }]
-  }
+    context: __dirname + '/app',
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './js/index.js'
+    ],
+    output: {
+        path: __dirname + '/app',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff" },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&minetype=application/font-woff" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=image/svg+xml" }
+        , {
+            test: /\.jsx?$/,
+            loaders: ['react-hot', 'babel'],
+            include: path.join(__dirname, 'app/js/')
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }, {
+            test: /\.scss$/,
+            loader: 'style!css!sass?sourceMap'
+        }]
+    }
 };
