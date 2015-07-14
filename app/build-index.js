42959aea228375723b293db012f019bd218b59fc
cdaringe <cdieringer@mrn.org>
2015-07-14 15:44:05 -0700

    restore build-index task

---
+++ app/build-index.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var jade = require('jade');
+var locals = {
+    pageTitle: 'COINSTAC'
+};
+
+if (process.env.COINS_ENV === 'development') {
+    locals.entryUrl = 'http://localhost:3000/bundle.js';
+} else {
+    locals.entryUrl = 'bundle.js';
+}
+
+// Complile the index template
+var fn = jade.compileFile('./app/index.jade', {
+    pretty: true
+});
+var html = fn(locals);
+
+fs.writeFileSync('./app/index.html', html, {encoding: 'utf8'});
