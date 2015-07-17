cd26c7b6bae33abcc838e8fe0636df4161ab62cc
Cory Reed <creed@mrn.org>
2015-07-17 12:11:07 -0700

    Fix 'home' navigation highlighting bug.

--- app/js/components/dashboard-nav.js
+++ app/js/components/dashboard-nav.js
@@ -3,12 +3,15 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { NavItemLink } from 'react-router-bootstrap';
+import Router from '../routes';
 
 export default class DashboardNav extends React.Component {
     render() {
+        const isHome = Router.getCurrentPath() === '/';
+
         return (
             <Nav bsStyle="pills" stacked>
-                <NavItemLink to="dashboard">
+                <NavItemLink to="dashboard" active={isHome}>
                     <span className="glyphicon glyphicon-home" aria-hidden="true"></span>
                     Home
                 </NavItemLink>
