b9ac24ae1d52af193f8c887347405870860a49df
cdaringe <cdieringer@mrn.org>
2015-09-01 11:43:39 -0700

    patch lifecycle state violation

--- app/js/components/home.js
+++ app/js/components/home.js
@@ -5,18 +5,19 @@ import * as allActions from '../actions/index';
 import { connect } from 'react-redux';
 
 let cachedActions;
-
-let init = false;
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.actions = null;
+    }
+    componentWillMount() {
+        cachedActions = cachedActions || bindActionCreators(allActions, this.props.dispatch);
+        cachedActions.init(); // trigger middlware state-change assertions, e.g. user authorized
+    }
     render() {
-        const actions = cachedActions || bindActionCreators(allActions, this.props.dispatch);
-        if (!init) {
-            actions.init(); // trigger middlware state-change assertions, e.g. user authorized
-            init = true;
-        }
         return (
             <div className="home">
-                <RouteHandler {...actions} {...this.props} />
+                <RouteHandler {...cachedActions} {...this.props} />
             </div>
         );
     }
