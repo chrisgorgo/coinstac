804e081ba2d2f478c217212c0a1f624c7f2e8690
cdaringe <cdieringer@mrn.org>
2015-09-22 21:19:55 -0700

    add base model to enforce defined attributes on instantiation

--- app/js/models/base.js
+++ app/js/models/base.js
@@ -1,13 +1,15 @@
+/**
+ * @package base.js - COINSTAC Model Extension
+ * using es5 for server compatibility (not that it has to run on the server, but we run `node file.js` to debug and test)
+ */
 'use strict';
 var Model = require('ampersand-model');
 module.exports = Model.extend({
     initialize: function(attrs) {
         for (var def in this._definition) {
-            if (def === '_rev') {
-                debugger;
-            }
             var defDfl = this._definition[def].default;
             if (this._definition[def].required &&
+                !this._derived[def] &&
                 (!defDfl || (typeof defDfl === 'function' && !defDfl())) &&
                 (attrs[def] === undefined || attrs[def] === null)) {
                 throw new ReferenceError('expected value for attr `' + def + '`. received ' + attrs[def]);
