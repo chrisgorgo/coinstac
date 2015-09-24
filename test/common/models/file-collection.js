4aa634d8c89c9330a5728641b07d8a7c74f95d41
cdaringe <cdieringer@mrn.org>
2015-09-23 17:15:46 -0700

    add file and file-collection tests

---
+++ test/common/models/file-collection.js
@@ -0,0 +1,36 @@
+'use strict';
+var _ = require('lodash');
+var path = require('path');
+var File = require(path.join(process.cwd(), 'app/js/models/file.js'));
+var FileCollection = require(path.join(process.cwd(), 'app/js/models/file-collection.js'));
+var test = require('tape');
+var moment = require('moment');
+
+var factory = function(opts) {
+    return function() {
+        return new File(opts);
+    };
+};
+
+var validOps = function() {
+    return {
+        filename: 'testFile',
+        dirname: '/test/dir',
+        sha: 'testSha',
+        tags: []
+    };
+};
+
+test('collection::file-collection - general', function(t) {
+    var f1 = factory(validOps())();
+    _.assign(f1, { filename: 'f1', sha: 'f1' });
+    var f2 = validOps(); // raw model form
+    _.assign(f2, { filename: 'f2', sha: 'f2' });
+    var modelSet = [f1, f2];
+    var collection = new FileCollection(modelSet);
+    t.ok(collection.models[0] instanceof File, 'File file in collection remains File');
+    t.ok(collection.models[1] instanceof File, 'raw file in collection converts to File');
+    t.equal(collection.models[0], collection.get(f1.sha, 'sha'), '`sha` index works');
+    t.equal(collection.models[1], collection.get('/test/dir/f2'), '`path` index works');
+    t.end();
+});
---
+++ test/common/models/file.js
@@ -0,0 +1,32 @@
+'use strict';
+var _ = require('lodash');
+var path = require('path');
+var File = require(path.join(process.cwd(), 'app/js/models/file.js'));
+var test = require('tape');
+
+var factory = function(opts) {
+    return function() {
+        return new File(opts);
+    };
+};
+
+var validOps = function() {
+    return {
+        filename: 'testFile',
+        dirname: '/test/dir',
+        sha: 'testSha',
+        tags: []
+    };
+};
+
+test('model::file - general', function(t) {
+    var f1 = factory(validOps())();
+    _.assign(f1, { filename: 'f1', sha: 'f1' });
+    var f2 = validOps(); // raw model form
+    _.assign(f2, { filename: 'f2', sha: 'f2' });
+
+    t.equal(f1.path, path.join(f1.dirname, f1.filename));
+    t.notEqual(f1.path, f2.path, 'unique paths generated');
+
+    t.end();
+});
