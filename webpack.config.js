87249ce7fb909e16b5f9919d98376792c312cdf9
cdaringe <cdieringer@mrn.org>
2015-09-24 17:40:18 -0700

    externalize all un-bundle-able packages

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,28 @@ var webpack = require('webpack');
 var chalk = require('chalk');
 var isDev = process.env.COINS_ENV === 'development';
 
+/**
+ * Get files that webpack will ignore from bundling, and expect to be available in the env
+ * @return {object} { packageName: 'commonjs packageName' } formatted entries
+ */
+var getExternals = function() {
+    var internals = [/react/]; // assert that all of these entries remain in webpack bundle
+    var externals =  {
+        fs: 'commonjs fs',
+        ipc: 'commonjs ipc',
+        remote: 'commonjs remote',
+        path: 'commonjs path',
+        config: 'commonjs config'
+    };
+    Object.keys(require('./package.json').dependencies).forEach(function(packageName) {
+        if (internals.some(function(internal) { return packageName.match(internal) })) {
+            return; // internal matched
+        }
+        externals[packageName] = 'commonjs ' + packageName
+    });
+    return externals;
+};
+
 module.exports = {
     context: __dirname + '/app',
     devtool: 'eval',
@@ -18,18 +40,7 @@ module.exports = {
         filename: 'bundle.js',
         publicPath: isDev ? 'http://localhost:3000/' : __dirname + '/app/build'
     },
-    externals: [
-        {
-            fs: 'commonjs fs',
-            ipc: 'commonjs ipc',
-            config: 'commonjs config',
-            sha: 'commonjs sha',
-            errio: 'commonjs errio',
-            path: 'commonjs path',
-            remote: 'commonjs remote',
-            pouchy: 'commonjs pouchy'
-        }
-    ],
+    externals: [ getExternals() ],
     plugins: [
         new webpack.NoErrorsPlugin()
     ].concat(isDev ? [
